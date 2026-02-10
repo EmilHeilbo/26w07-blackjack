11d6f7052288f37f82dcb301681dcab58f77baa6
Emil Heilbo <emil@heilbo.dev>
2026-02-10 09:18:58 +0100

    feat(card): improve class definition, add missing return types

--- src/logic.py
+++ src/logic.py
@@ -25,21 +25,30 @@ class card:
   Number = Enum("Number", CARD_NAMES)
 
   def __init__(self, set: Set = Set(1), number: Number = Number(1)) -> None:
-    self.set = set
-    self.number = number
+    self.set = set if type(set) is self.Set else self.Set(set)
+    self.number = number if type(number) is self.Number else self.Number(number)
 
-  def __repr__(self):
-    return "(%s,%s)" % (self.set, self.number)
+  def __repr__(self) -> str:
+    return "%s(%s,%s)" % (self.__class__, self.number.value, self.set.value)
 
   def __str__(self) -> str:
     return "%s of %s" % (self.number.name, self.set.name)
 
+  # Implement int conversion to simplify sorting
+  def __int__(self) -> int:
+    return self.set.value * 13 + self.number.value
 
-def get_deck(number_of_decks: int = 1, shuffle_deck: bool = True):
-  deck = []
+  def __lt__(self, other) -> bool:
+    return int(self) < int(other)
+
+  def __eq__(self, other) -> bool:
+    return int(self) == int(other)
+
+
+def get_deck(number_of_decks: int = 1, shuffle_deck: bool = True) -> list[card]:
+  deck: list[card] = []
   for s in card.Set:
     for n in card.Number:
-      print(f"Adding {card(s, n)}")
       deck.append(card(s, n))
   for _ in range(1, number_of_decks):
     deck += deck[:52]
