7913b7ffdd173a010fe71cb63cd798776b00169a
Emil Heilbo <emil@heilbo.dev>
2026-02-16 08:23:16 +0100

    fix: ensure only one occurrence of the possible launch args

--- src/__main__.py
+++ src/__main__.py
@@ -7,16 +7,26 @@ from .view.console import ConsoleView
 
 def main(args: list[str] = []):
   INTERFACE: ConsoleView | None
+  LAUNCH_ARGS = ["--cli", "--gui", "--web"]
+  state = Game_State()
+
   logging.basicConfig(level=logging.INFO, format="%(message)s")
   logging.debug("Arguments: %s", ", ".join(args))
-  state = Game_State()
+  _matches = 0
+  for arg in LAUNCH_ARGS:
+    if arg in args:
+      _matches += 1
+  if _matches > 1:
+    logging.error(f"Multiple launch arguments found: {args}")
+    logging.error("Please launch with only one of '--cli', '--gui' or '--web'.")
+    return 1
   match args:
-    case ["--cli"]:
+    case _ if LAUNCH_ARGS[0] in args:
       INTERFACE = ConsoleView(state)
-    case ["--gui"]:
+    case _ if LAUNCH_ARGS[1] in args:
       logging.error("GUI interface is not implemented yet.")
       return 1
-    case ["--web"]:
+    case _ if LAUNCH_ARGS[2] in args:
       logging.error("Web interface is not implemented yet.")
       return 1
     case _:
