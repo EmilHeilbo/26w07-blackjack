f6a1f445d772ca99fb5011af8e3e66a2ca26ca2a
Emil Heilbo <emil@heilbo.dev>
2026-02-20 09:22:47 +0100

    chore(web): add initial html files, change example code

--- src/blackjack/api.py
+++ src/blackjack/api.py
@@ -1,53 +1,63 @@
 # Example code from https://fastapi.tiangolo.com/advanced/websockets/
 
-from fastapi import FastAPI, WebSocket
+import logging
+from typing import Annotated
+
+from fastapi import (
+  Cookie,
+  Depends,
+  FastAPI,
+  Query,
+  WebSocket,
+  WebSocketException,
+  status,
+)
 from fastapi.responses import HTMLResponse
 
 api = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
 
 @api.get("/")
 async def get():
-  return HTMLResponse(html)
+  try:
+    with open("src/web/index.html", "r") as f:
+      html = f.read()
+      logging.info("f.read() from html:")
+      logging.info(html)
+      f.close()
+      return HTMLResponse(html)
+  except OSError as e:
+    logging.fatal(f"Error reading lobby.html: {e}")
+    exit(-1)
+  except Exception as e:
+    logging.fatal(f"Unexpected error: {e}")
+    exit(-1)
 
 
-@api.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def get_cookie_or_token(
+  websocket: WebSocket,
+  session: Annotated[str | None, Cookie()] = None,
+  token: Annotated[str | None, Query()] = None,
+):
+  if session is None and token is None:
+    raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+  return session or token
+
+
+@api.websocket("/items/{item_id}/ws")
+async def websocket_endpoint(
+  *,
+  websocket: WebSocket,
+  item_id: str,
+  q: int | None = None,
+  cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+):
   await websocket.accept()
   while True:
     data = await websocket.receive_text()
-    await websocket.send_text(f"Message text was: {data}")
+    await websocket.send_text(
+      f"Session cookie or query token value is: {cookie_or_token}"
+    )
+    if q is not None:
+      await websocket.send_text(f"Query parameter q is: {q}")
+    await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
